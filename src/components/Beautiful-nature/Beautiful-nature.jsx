46df2f25e4e0c4c3e065d11dd6983c1d1d41741c
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2026-02-17 22:05:01 +0200

    animation

--- src/components/Beautiful-nature/Beautiful-nature.jsx
+++ src/components/Beautiful-nature/Beautiful-nature.jsx
@@ -6,10 +6,39 @@ export const BeautifulNature = () => {
       <div className="container">
         <h2 className={styles["nature-title"]}>Beautiful nature</h2>
         <div className={styles["nature-container"]}>
+          <img className={styles["nature-img"]} src="/images/sea.png" alt="" />
+          <img className={styles["nature-img"]} src="/images/lake.png" alt="" />
           <img
             className={styles["nature-img"]}
-            src="/images/beautiful-nature.png"
-            alt="Beautiful nature"
+            src="/images/forest.png"
+            alt=""
+          />
+          <img
+            className={styles["nature-img"]}
+            src="/images/hills.png"
+            alt=""
+          />
+          <img
+            className={styles["nature-img"]}
+            src="/images/evening-lake.png"
+            alt=""
+          />
+          <img className={styles["nature-img"]} src="/images/sea.png" alt="" />
+          <img className={styles["nature-img"]} src="/images/lake.png" alt="" />
+          <img
+            className={styles["nature-img"]}
+            src="/images/forest.png"
+            alt=""
+          />
+          <img
+            className={styles["nature-img"]}
+            src="/images/hills.png"
+            alt=""
+          />
+          <img
+            className={styles["nature-img"]}
+            src="/images/evening-lake.png"
+            alt=""
           />
         </div>
       </div>
